787f464fdae21b2a8823a222b30f39013d584702
solkirans <mert.solkiran@gmail.com>
2023-02-04 01:05:53 +0100

    Docker compose
    Docker compose

--- Installation/dockerInstall.sh
+++ Installation/dockerInstall.sh
@@ -4,12 +4,17 @@
 
 # https://www.homeautomationguy.io/home-assistant-tips/installing-docker-home-assistant-and-portainer-on-ubuntu-linux/
 
+
+# Remove previous Docker installations
 sudo apt-get remove docker 
 sudo apt-get remove docker-engine 
 sudo apt-get remove docker.io 
 sudo apt-get remove containerd 
 sudo apt-get remove runc
 
+
+
+# Install Docker
 sudo apt-get update
 
 sudo apt-get install ca-certificates
@@ -29,3 +34,11 @@ sudo apt-get update
 sudo apt-get install docker-ce
 sudo apt-get install docker-ce-cli
 sudo apt-get install containerd.io
+
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+docker-compose up -d 
